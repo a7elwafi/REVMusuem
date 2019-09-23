58fb2acf5c797bde16dc5eee88de4f4bdf5c888f
eric <maisel@enib.fr>
2019-09-23 15:41:11 +0200

    modif mineure .

--- index.js
+++ index.js
@@ -1,13 +1,44 @@
 
 
-function initThree(){
+function init(){
 	initRenderer() ; 
 	initCamera() ; 
 	initScene() ; 
 }
 
-function initScene(){}
-
-function animer(){}
+function animer(){
+	requestAnimationFrame(animer) ; 
+	update() ; 
+	render() ; 
+}
 
 // =========================================================================
+
+function initRenderer(){
+	_largeur = window.innerWidth ; 
+	_hauteur = window.innerHeight ; 
+	_renderer = new THREE.WebGLRenderer({antialias:true, alpha:true}) ;
+	_renderer.setSize(_largeur, _hauteur) ; 
+}
+
+function initCamera(){
+	_camera = new THREE.PerspectiveCamera(
+			45.0,
+			_largeur / _hauteur,
+			0.1, 1000.0) ;
+	_controleur = new KeyboardControls(_camera) ;  
+}
+
+function initScene(){
+}
+
+// =========================================================================
+
+function update(){
+	var dt = 0.001 ; 
+	_controleur.update(dt) ; 
+}
+
+function render(){
+	_renderer.render(_scene, _camera) ; 
+}
